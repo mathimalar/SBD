5134c85a88dfd2b10ab95b7a0c6340f3dc321512
Dror Harush - Lab <drorharush@gmail.com>
2022-01-27 17:05:23 +0200

    put the NN layers in one file

--- activation_model.py
+++ activation_model.py
@@ -10,6 +10,7 @@ import model_tools
 from dataset import QPIDataSet
 from matplotlib import pyplot as plt
 import numpy as np
+from model_tools import Up, Down
 
 loss_func = ActivationLoss(0.01)
 
@@ -141,70 +142,4 @@ class ActivationSys(pl.LightningModule):
         return pred_activation
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-        return optimizer
-
-
-class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) *
-    """
-
-    def __init__(self, in_channels, out_channels, mid_channels=None):
-        super().__init__()
-        if not mid_channels:
-            mid_channels = out_channels
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=(3, 3), padding=1),
-            nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.1),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=(3, 3), padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.1)
-        )
-
-    def forward(self, x):
-        return self.double_conv(x)
-
-
-class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
-
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2),
-            DoubleConv(in_channels, out_channels)
-        )
-
-    def forward(self, x):
-        return self.maxpool_conv(x)
-
-
-class OutConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(OutConv, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1))
-
-    def forward(self, x):
-        return self.conv(x)
-
-
-class Up(nn.Module):
-    """Upscaling then double conv"""
-
-    def __init__(self, in_channels, out_channels, bilinear=True):
-        super().__init__()
-
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=(2, 2), stride=(2, 2))
-            self.conv = DoubleConv(in_channels, out_channels)
-
-    def forward(self, x):
-        x = self.up(x)
-        return self.conv(x)
+        return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
--- model.py
+++ model.py
@@ -5,100 +5,7 @@ import torch.nn as nn
 from torch.nn import Conv2d, ReLU, MaxPool2d, Linear, BatchNorm2d, LeakyReLU, Softmax
 import torch.nn.functional as F
 import torch
-
-
-class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) *
-    """
-
-    def __init__(self, in_channels, out_channels, mid_channels=None):
-        super().__init__()
-        if not mid_channels:
-            mid_channels = out_channels
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=(3, 3), padding=1),
-            nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.1),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=(3, 3), padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.1)
-        )
-
-    def forward(self, x):
-        return self.double_conv(x)
-
-
-class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
-
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2),
-            DoubleConv(in_channels, out_channels)
-        )
-
-    def forward(self, x):
-        return self.maxpool_conv(x)
-
-
-class OutConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(OutConv, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1))
-
-    def forward(self, x):
-        return self.conv(x)
-
-
-class Up(nn.Module):
-    """Upscaling then double conv"""
-
-    def __init__(self, in_channels, out_channels, bilinear=True):
-        super().__init__()
-
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=(2, 2), stride=(2, 2))
-            self.conv = DoubleConv(in_channels, out_channels)
-
-    def forward(self, x):
-        x = self.up(x)
-        return self.conv(x)
-
-
-class UpCombine(nn.Module):
-    """Upscaling then double conv"""
-
-    def __init__(self, in_channels, out_channels, bilinear=True):
-        super().__init__()
-
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=(2, 2), stride=(2, 2))
-            self.conv = DoubleConv(in_channels, out_channels)
-
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-        x = torch.cat([x2, x1], dim=1)
-        return self.conv(x)
+from model_tools import Up, Down
 
 
 class ActivationNet(nn.Module):
@@ -115,7 +22,7 @@ class ActivationNet(nn.Module):
         self.up2 = Up(2 ** 4, 2 ** 2)
         pad = 2
         ker_same = (5, 5)
-        self.features = nn.Sequential(
+        self.encoder_decoder = nn.Sequential(
 
             # Input layer
             Conv2d(input_channels, 2 ** 2, kernel_size=ker_same, padding=pad, bias=False),
@@ -139,105 +46,10 @@ class ActivationNet(nn.Module):
         )
 
     def forward(self, x_in):
-        # x = (x_in - torch.min(x_in)) / torch.max(x_in)
         x = model_tools.normalize_tensor_0to1(x_in)
-        x = self.features(x)
+        x = self.encoder_decoder(x)
         x = model_tools.normalize_tensor_sumto1(x)
         return x
-
-
-class ActivationResNet(nn.Module):
-    def __init__(self, input_channels=1):
-        super().__init__()
-        self.input_channels = input_channels
-        self.down1 = Down(2 ** 2, 2 ** 3)
-        self.down2 = Down(2 ** 3, 2 ** 4 // 2)
-        self.up1 = UpCombine(2 ** 4, 2 ** 3 // 2)
-        self.up2 = UpCombine(2 ** 3, 2 ** 2)
-        pad = 2  # These values make sure the image dimensions stay the same
-        ker_same = (5, 5)
-        self.input = nn.Sequential(
-            Conv2d(input_channels, 2 ** 2, kernel_size=ker_same, padding=pad, bias=False),
-            BatchNorm2d(2 ** 2),
-            LeakyReLU(),
-        )
-        self.output = nn.Sequential(
-            Conv2d(2 ** 2, 1, kernel_size=ker_same, padding=pad, bias=False),
-            ReLU()
-        )
-
-    def forward(self, x_in):
-        x = (x_in - torch.min(x_in)) / torch.max(x_in)
-        x1 = self.input(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x = self.up1(x3, x2)
-        x = self.up2(x, x1)
-        x = self.output(x)
-        x = model_tools.normalize_tensor_sumto1(x)
-        return x
-
-
-class ActivationSmiResNet(nn.Module):
-    def __init__(self, input_channels=1):
-        super().__init__()
-        self.input_channels = input_channels
-        self.down1 = Down(2 ** 2, 2 ** 3)
-        self.down2 = Down(2 ** 3, 2 ** 4 // 2)
-        self.up1 = UpCombine(2 ** 4, 2 ** 3)
-        self.up2 = Up(2 ** 3, 2 ** 2)
-        pad = 2  # These values make sure the image dimensions stay the same
-        ker_same = (5, 5)
-        self.input = nn.Sequential(
-            Conv2d(input_channels, 2 ** 2, kernel_size=ker_same, padding=pad, bias=False),
-            BatchNorm2d(2 ** 2),
-            LeakyReLU(),
-        )
-        self.output = nn.Sequential(
-            Conv2d(2 ** 2, 1, kernel_size=ker_same, padding=pad, bias=False),
-            ReLU()
-        )
-
-    def forward(self, x_in):
-        x = (x_in - torch.min(x_in)) / torch.max(x_in)
-        x1 = self.input(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x = self.up1(x3, x2)
-        x = self.up2(x)
-        x = self.output(x)
-        x = model_tools.normalize_tensor_sumto1(x)
-        return x
-
-
-class KerNet(nn.Module):
-    """
-    Cnn that shrinks by factor 8 and keeps the channel number the same.
-    """
-
-    def __init__(self):
-        super(KerNet, self).__init__()
-        # Doesn't change the size of the image
-        self.filter_expand = nn.Sequential(
-            DoubleConv(1, 2 ** 6),
-        )
-        # shrinks transverse size by a factor of 2.
-        self.down1 = Down(2 ** 6, 2 ** 7)
-        self.down2 = Down(2 ** 7, 2 ** 8)
-        self.down3 = Down(2 ** 8, 2 ** 9)
-        factor = 2
-        self.up1 = Up(2 ** 9, 2 ** 8)
-        self.up2 = Up(2 ** 8, 2 ** 7)
-        self.out = OutConv(2 ** 7, 1)
-
-    def forward(self, x):
-        expanded = self.filter_expand(x)
-        x = self.down1(expanded)
-        x = self.down2(x)
-        x = self.down3(x)
-        x = self.up1(x)
-        x = self.up2(x)
-        return self.out(x)
 
 
 class LISTA(nn.Module):
@@ -271,4 +83,98 @@ class LISTA(nn.Module):
         return x
 
 
+# Failed networks:
 
+
+# class KerNet(nn.Module):
+#     """
+#     Cnn that shrinks by factor 8 and keeps the channel number the same.
+#     """
+#
+#     def __init__(self):
+#         super(KerNet, self).__init__()
+#         # Doesn't change the size of the image
+#         self.filter_expand = nn.Sequential(
+#             DoubleConv(1, 2 ** 6),
+#         )
+#         # shrinks transverse size by a factor of 2.
+#         self.down1 = Down(2 ** 6, 2 ** 7)
+#         self.down2 = Down(2 ** 7, 2 ** 8)
+#         self.down3 = Down(2 ** 8, 2 ** 9)
+#         factor = 2
+#         self.up1 = Up(2 ** 9, 2 ** 8)
+#         self.up2 = Up(2 ** 8, 2 ** 7)
+#         self.out = OutConv(2 ** 7, 1)
+#
+#     def forward(self, x):
+#         expanded = self.filter_expand(x)
+#         x = self.down1(expanded)
+#         x = self.down2(x)
+#         x = self.down3(x)
+#         x = self.up1(x)
+#         x = self.up2(x)
+#         return self.out(x)
+
+
+# class ActivationResNet(nn.Module):
+#     def __init__(self, input_channels=1):
+#         super().__init__()
+#         self.input_channels = input_channels
+#         self.down1 = Down(2 ** 2, 2 ** 3)
+#         self.down2 = Down(2 ** 3, 2 ** 4 // 2)
+#         self.up1 = UpCombine(2 ** 4, 2 ** 3 // 2)
+#         self.up2 = UpCombine(2 ** 3, 2 ** 2)
+#         pad = 2  # These values make sure the image dimensions stay the same
+#         ker_same = (5, 5)
+#         self.input = nn.Sequential(
+#             Conv2d(input_channels, 2 ** 2, kernel_size=ker_same, padding=pad, bias=False),
+#             BatchNorm2d(2 ** 2),
+#             LeakyReLU(),
+#         )
+#         self.output = nn.Sequential(
+#             Conv2d(2 ** 2, 1, kernel_size=ker_same, padding=pad, bias=False),
+#             ReLU()
+#         )
+#
+#     def forward(self, x_in):
+#         x = (x_in - torch.min(x_in)) / torch.max(x_in)
+#         x1 = self.input(x)
+#         x2 = self.down1(x1)
+#         x3 = self.down2(x2)
+#         x = self.up1(x3, x2)
+#         x = self.up2(x, x1)
+#         x = self.output(x)
+#         x = model_tools.normalize_tensor_sumto1(x)
+#         return x
+
+
+# class ActivationSmiResNet(nn.Module):
+#     def __init__(self, input_channels=1):
+#         super().__init__()
+#         self.input_channels = input_channels
+#         self.down1 = Down(2 ** 2, 2 ** 3)
+#         self.down2 = Down(2 ** 3, 2 ** 4 // 2)
+#         self.up1 = UpCombine(2 ** 4, 2 ** 3)
+#         self.up2 = Up(2 ** 3, 2 ** 2)
+#         pad = 2  # These values make sure the image dimensions stay the same
+#         ker_same = (5, 5)
+#         self.input = nn.Sequential(
+#             Conv2d(input_channels, 2 ** 2, kernel_size=ker_same, padding=pad, bias=False),
+#             BatchNorm2d(2 ** 2),
+#             LeakyReLU(),
+#         )
+#         self.output = nn.Sequential(
+#             Conv2d(2 ** 2, 1, kernel_size=ker_same, padding=pad, bias=False),
+#             ReLU()
+#         )
+#
+#     def forward(self, x_in):
+#         x = (x_in - torch.min(x_in)) / torch.max(x_in)
+#         x1 = self.input(x)
+#         x2 = self.down1(x1)
+#         x3 = self.down2(x2)
+#         x = self.up1(x3, x2)
+#         x = self.up2(x)
+#         x = self.output(x)
+#         x = model_tools.normalize_tensor_sumto1(x)
+#         return x
